c572e213d56273a928c22415710e7ce40cf69ddf
Qadzif Kamil Zahari <jipi@baskit.app>
2024-05-17 16:48:50 +0700

    chore(eslint): add stylistic rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import stylisticJs from '@stylistic/eslint-plugin-js';
 
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/semi': ['error', 'always', {
+        omitLastInOneLineBlock: true,
+      }],
+    },
+  },
 ];
